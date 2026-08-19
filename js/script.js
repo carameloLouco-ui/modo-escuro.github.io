dfc6fd78cba19e95a47dd2ede4137d221d026a43
carameloLouco-ui <davi.gaia@escola.pr.gov.br>
2026-08-19 08:36:28 -0300

    Implement dark mode toggle and text alteration
    Added functionality for dark mode toggle and text change.

--- js/script.js
+++ js/script.js
@@ -1,1 +1,20 @@
+//modo escuro
+const botão Tema = document.getElementById("botaoTema");
 
+function alternarTema(){
+  document.body.classList.toggle("escuro");
+  (document.body.classList.contains("escuro")){
+    botão Tema.textContent="Modo claro";
+  }else{
+    botão Tema.textContent="Modo escuro";
+  }
+}
+botão Tema.addEventListener("click", alternarTema);
+
+//alterar texto
+const botaoMensagem = document.getElementById("mensagem");
+const mensagem = document.getElementById("mensagem");
+function alterarTexto(){
+  mensagem.textContent = "O texto foi alterado pelo JavaScript!";
+}
+botão Mensagem.addEventListener("click", alterarTexto);
