816c780aa34320d12d07a4a5997f140a3852964f
carameloLouco-ui <davi.gaia@escola.pr.gov.br>
2026-08-19 08:55:23 -0300

    Refactor theme toggle and text change functions

--- js/script.js
+++ js/script.js
@@ -2,39 +2,34 @@
 // MODO ESCURO
 // =============================
 
-const botaoTema =
-document.getElementById("botaoTema");
+const botaoTema = document.getElementById("botaoTema");
 
 function alternarTema() {
 
-document.body.classList.toggle("escuro");
+  document.body.classList.toggle("escuro");
 
-if (document.body.classList.contains("escuro")) {
+  if (document.body.classList.contains("escuro")) {
 
-botaoTema.textContent = "☀ Modo claro";
+    botaoTema.textContent = "☀ Modo claro";
 
-} else {
+  } else {
 
-botaoTema.textContent = "🌙 Modo escuro";
+    botaoTema.textContent = "🌙 Modo escuro";
 
-}
+  }
 }
 
 // =============================
 // ALTERAR TEXTO
 // =============================
 
-const botaoMensagem =
-document.getElementById("botaoMensagem");
+const botaoMensagem = document.getElementById("botaoMensagem");
 
-const mensagem =
-document.getElementById("mensagem");
+const mensagem = document.getElementById("mensagem");
 
 function alterarTexto() {
 
-mensagem.textContent =
-"O texto foi alterado pelo JavaScript!";
-
+  mensagem.textContent = "O texto foi alterado pelo JavaScript!";
 }
 
 botaoMensagem.addEventListener("click", alterarTexto);
